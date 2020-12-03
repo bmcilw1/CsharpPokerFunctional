7b5ae9821b626cbda06f0b9850008b21df3757aa
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-12-03 10:47:44 -0600

    Initial commit

---
+++ Card.cs
@@ -0,0 +1,32 @@
+public record Card(CardValue Value, CardSuit Suit)
+{
+    public override string ToString()
+    {
+        return $"{Value} of {Suit}";
+    }
+};
+
+public enum CardValue
+{
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
+public enum CardSuit
+{
+    Spades,
+    Diamonds,
+    Clubs,
+    Hearts
+}
---
+++ Tests/CardTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace CsharpPokerFunctional
+{
+    public class CardTests
+    {
+        [Fact]
+        public void CanCreateCard()
+        {
+            var card = new Card(CardValue.Ace, CardSuit.Diamonds);
+            Assert.NotNull(card);
+        }
+
+        [Fact]
+        public void CanCreateCardWithValue()
+        {
+
+            var card = new Card(CardValue.Ace, CardSuit.Clubs);
+
+            Assert.Equal(CardValue.Ace, card.Value);
+            Assert.Equal(CardSuit.Clubs, card.Suit);
+        }
+
+        [Fact]
+        public void CanDescribeCard()
+        {
+            var card = new Card(CardValue.Ace, CardSuit.Spades);
+
+            Assert.Equal("Ace of Spades", card.ToString());
+        }
+    }
+}
